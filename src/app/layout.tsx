89ada346bbd9b1ebfab7cb881847de7ee7d0e328
Swapnanil <adioticswapnanil@gmail.com>
2024-11-18 12:37:41 +0530

    created landing page ui

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,11 +24,15 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="h-full">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased relative h-full`}
       >
-        {children}
+        <main className="relative flex flex-col min-h-screen">
+         <div className="flex-grow flex-1">
+          {children}
+         </div>
+        </main>
       </body>
     </html>
   );
